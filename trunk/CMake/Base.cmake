f9967e64aa481ff3dc553bb879899cfc2e2ad031
madelmann <madelmann@a1cf54fd-c598-4065-951b-5e5a467d183e>
2016-04-24 10:38:31 +0000

    added docs; cosmetic changes to CMake/Base.cmake

--- trunk/CMake/Base.cmake
+++ trunk/CMake/Base.cmake
@@ -14,14 +14,15 @@ endif()
 SET(EXECUTABLE_OUTPUT_PATH ${PROJECT_BINARY_DIR}/bin)   # binary root
 SET(LIBRARY_OUTPUT_PATH ${PROJECT_BINARY_DIR}/lib)      # library root
 
-#SET(CMAKE_CXX_FLAGS "-ansi -pedantic -fPIC -Wall -Werror -Wunused -Wno-long-long -Wnon-virtual-dtor") # -std=c++0x ;-(
 SET(CMAKE_CXX_FLAGS "-pedantic -fPIC -Wall -Werror -Wunused -Wno-long-long -Wnon-virtual-dtor -std=c++11")
 
 if( "${BUILD}" STREQUAL "" OR "${BUILD}" MATCHES "Debug")
 
     # by default we build debug!
-    # MESSAGE("Setting default build to: Debug")
+    MESSAGE("Setting build to: Debug")
+
     SET(CMAKE_BUILD_TYPE "Debug")
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wextra")
 
     # do more logging
     add_definitions(-DDEBUG)
@@ -29,18 +30,15 @@ if( "${BUILD}" STREQUAL "" OR "${BUILD}" MATCHES "Debug")
     # suppress "veraltete Konvertierung von Zeichenkettenkonstante in »char*« [-Werror=write-strings]"
     add_definitions(-Wno-write-strings)
 
-    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wextra")
-
 elseif("${BUILD}" MATCHES "Release")
 
-    # MESSAGE("Setting default build to: Release")
+    MESSAGE("Setting build to: Release")
+
     SET(CMAKE_BUILD_TYPE "Release")
-    SET(CMAKE_CXX_FLAGS "-std=c++11 -O3")
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-strict-aliasing -O3")
 
     # suppress "veraltete Konvertierung von Zeichenkettenkonstante in »char*« [-Werror=write-strings]"
     add_definitions(-Wno-write-strings)
-
-    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-strict-aliasing")
 
 else()
 

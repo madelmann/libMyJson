73c4d90131e72fb7ee44e484e4c73b915b2af745
madelmann <madelmann@a1cf54fd-c598-4065-951b-5e5a467d183e>
2016-12-03 15:23:00 +0000

    updated release script

--- trunk/make-release.sh
+++ trunk/make-release.sh
@@ -1,10 +1,21 @@
 
-EXCLUDE=*.sh
+EXCLUDES="--exclude=*.sh --exclude=cmake-build*"
 RELEASE="libMyJson-`cat version`_`svnversion`.tar.gz"
 
 echo "Packing files for release ${RELEASE} ..."
 
-tar cfvz ${RELEASE} --exclude=${EXCLUDE} *
+tar cfvz ${RELEASE} ${EXCLUDES} *
 
+echo ""
+echo "Uploading files..."
+
+scp ${RELEASE} README.txt madelmann@frs.sourceforge.net:/home/frs/project/libmyjson/
+
+echo ""
+echo "Cleanup..."
+
+rm ${RELEASE}
+
+echo ""
 echo "done."
 
